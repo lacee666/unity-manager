640e493f3bdf8c9d5c16737fa8148444adb33bb3
Laszlo Bulatovic <laci96zenta@gmail.com>
2018-04-14 14:22:50 +0200

    facebook and menu work

---
+++ UnityTest/Assets/scripts/InGameMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour {
+
+    private GameObject menu;
+    private bool menuOn;
+    public void Start()
+    {
+        menuOn = false;
+        menu = GameObject.Find("menu");
+        menu.SetActive(false);
+    }
+    public void Cancel()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !menuOn)
+        {
+            Time.timeScale = 0;
+            menu.SetActive(true);
+        } else if(Input.GetKeyDown(KeyCode.Escape) && menuOn)
+        {
+            Time.timeScale = 1;
+            menu.SetActive(false);
+        }
+    }
+    public void BackToMenu()
+    {
+        Application.LoadLevel("menu");
+    }
+}
---
+++ UnityTest/Assets/scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Facebook.Unity;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour {
+
+
+    public void Start()
+    {
+ 
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("test");
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+    /*
+    public void KeyBoard()
+    {
+        main.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        main.SetActive(true);
+        options.SetActive(false);
+    }
+    */
+}
